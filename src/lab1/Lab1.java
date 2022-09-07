254cbb93732e1059cc2379fcf704975318d1bc11
shinzenatt <42674003+ShinzenATT@users.noreply.github.com>
2022-09-07 18:50:27 +0200

    Setup lab1

---
+++ src/lab1/Lab1.java
@@ -0,0 +1,18 @@
+package lab1;
+
+import TSim.*;
+
+public class Lab1 {
+
+  public Lab1(int speed1, int speed2) {
+    TSimInterface tsi = TSimInterface.getInstance();
+
+    try {
+      tsi.setSpeed(1,speed1);
+    }
+    catch (CommandException e) {
+      e.printStackTrace();    // or only e.getMessage() for the error
+      System.exit(1);
+    }
+  }
+}
